dbfbf478cd5109325009e965670cebfd95797fb7
Containerxox <containerxox@gmail.com>
2026-02-25 16:43:32 +0900

    Add step01DI - 필드 주입

--- step01-di/src/main/java/com/spring/di/controller/DIController.java
+++ step01-di/src/main/java/com/spring/di/controller/DIController.java
@@ -11,6 +11,10 @@ import lombok.RequiredArgsConstructor;
 @Component
 public class DIController {
 	
+	// 필드 주입 v1
+	@Autowired
+	private DIService diService;
+	
 	// 생성자 주입 v2
 	private final DIService disService;
 	
@@ -22,6 +26,7 @@ public class DIController {
 //		System.out.println("Controller ===> Service: "+ diService); //DIService의 객체정보를 출력
 //	}
 	
+	// 기존 방식
 //	private DIService service = new DIService(); //이제 필요 없다.
 	
 }
