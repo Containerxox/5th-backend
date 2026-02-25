a5dc650bf2427588d64130682c943bc402d26c2b
Containerxox <containerxox@gmail.com>
2026-02-25 17:09:41 +0900

    Add step01DI - @Bean

--- step01-di/src/main/java/com/spring/di/service/DIService.java
+++ step01-di/src/main/java/com/spring/di/service/DIService.java
@@ -1,5 +1,6 @@
 package com.spring.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,4 +9,7 @@ public class DIService {
 	public DIService() {
 		System.out.println("DIService 생성자");
 	}
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 }
