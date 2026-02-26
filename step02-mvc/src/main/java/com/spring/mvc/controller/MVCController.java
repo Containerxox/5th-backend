ca1a919960f0de0c9331abcb8b3d81d0f7967d56
Containerxox <containerxox@gmail.com>
2026-02-26 11:49:39 +0900

    URL쿼리필드명과 객체의필드명이 같은 경우, 자동 바인딩

--- step02-mvc/src/main/java/com/spring/mvc/controller/MVCController.java
+++ step02-mvc/src/main/java/com/spring/mvc/controller/MVCController.java
@@ -2,6 +2,7 @@ package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -39,23 +40,31 @@ public class MVCController {
 	// "{"sid"="web-mvc", "grade"="junior"}"
 
 	
-	// http://localhost:8080/test5?sid=web-mvc&grade=junior
+	// http://localhost:8080/test5?sname=web-mvc&grade=junior
+	// 필드가 여러개면 복잡해져! => 스프링 부트가 자동으로 매핑
+	// URL 쿼리 필드와 객체의 필드 이름이 서로 같다면, 스프링부트가 알아서 매핑해줌.
+	@GetMapping(value = "/test5")
+	public void test5(@ModelAttribute Student student) { //@ModelAttribute 생략가능
+		System.out.println("MVCController : test5()");
+		System.out.println(student);
+	}
+	
 
 	
-	// http://localhost:8080/test4?sid=web-mvc&grade=junior
+	// http://localhost:8080/test4?sname=web-mvc&grade=junior
 	// 데이터를 받아오고 객체로 만들고 싶어!
 	/*
 	 * Student
-	 * String sid
+	 * String sname
 	 * String grade
 	 * 
 	 */
 	@GetMapping(value = "/test4")
-	public void test4(String sid, String grade) {
+	public void test4(String sname, String grade) {
 		System.out.println("MVCController : test4()");
-		System.out.println("sid : "+ sid + ", " + "grade : " + grade);
+		System.out.println("sid : "+ sname + ", " + "grade : " + grade);
 		
-		Student student = new Student(1, sid, grade);
+		Student student = new Student(1, sname, grade);
 		System.out.println(student);
 		
 	}
