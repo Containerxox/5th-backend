3813313ebfaf54dc0b8c3278164258d0d42379e5
Containerxox <containerxox@gmail.com>
2026-02-27 11:09:42 +0900

    폼을 통해 제출한 항목의 값들을 화면에 출력

--- step03-thymeleaf/src/main/java/com/spring/mvc/controller/FormController.java
+++ step03-thymeleaf/src/main/java/com/spring/mvc/controller/FormController.java
@@ -1,18 +1,23 @@
 package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.spring.mvc.dto.Student;
+import com.spring.mvc.dto.Student.Grade;
 
 
 @Controller
 public class FormController {
 	
 	@GetMapping(value="/form")
-	public String form(Student student) {
+	public String form(Student student, Model model) {
 		System.out.println("FormController");
+		student.setGrade(Grade.JUNIOR);
 		System.out.println(student);
+		
+		model.addAttribute("student", student);
 		
 		return "test";
 	}
