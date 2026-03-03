95391dfb117464a74d30e382b148658c9e66c303
Containerxox <containerxox@gmail.com>
2026-03-03 11:20:27 +0900

    Add step05-spring-data-jpa

---
+++ step05-spring-data-jpa/src/main/java/com/spring/jpa/service/DeptService.java
@@ -0,0 +1,14 @@
+package com.spring.jpa.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptService { // Service는 "비즈니스 로직 처리" + "트랜잭션 처리" 담당
+	
+	public Object jpaTest() {
+		Object result = null;
+		
+		return result;
+	}
+	
+}
