e4db298ceec8c8893a26a8cf0480bd8945b2e63f
Containerxox <containerxox@gmail.com>
2026-03-03 17:19:36 +0900

    Add step07-rest-api(@RestController로 변경)

--- step07-rest-api/src/main/java/com/spring/api/controller/StudentController.java
+++ step07-rest-api/src/main/java/com/spring/api/controller/StudentController.java
@@ -1,13 +1,18 @@
 package com.spring.api.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.api.service.StudentService;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@Controller
+@RestController  // @Controller + @ResponseBody
 public class StudentController {
+	
 	private final StudentService studentService;
+	
+	
+	
+	
 }
