61dd04bf3b4ba1ba744780a6d60c2a57870b86c6
Containerxox <containerxox@gmail.com>
2026-03-04 11:08:51 +0900

    Add step06-spring-data-jpa-apply (Entity Graph)

--- step06-spring-data-jpa-apply/src/main/java/com/spring/jpa/repository/LectureRepository.java
+++ step06-spring-data-jpa-apply/src/main/java/com/spring/jpa/repository/LectureRepository.java
@@ -2,6 +2,7 @@ package com.spring.jpa.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -16,5 +17,9 @@ public interface LectureRepository extends JpaRepository<Lecture, Long>{
 
 	@Query("select l from Lecture l left join fetch l.students")
 	List<Lecture> findAllWithOuterFetchJoin();
+
+	@EntityGraph(attributePaths = {"students"}) //엔터티의 필드값을 적어주면 됨. // @EntityGraph를 통해 students 필드에 대한 내용을 join을 해서 한번에 가져옴.
+	@Query("select l from Lecture l ")
+	List<Lecture> findAllWithEntityGraph();
 	
 }
