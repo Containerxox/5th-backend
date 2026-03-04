f6c8dab34d8336e274c032c76c548a1b6e25f461
Containerxox <containerxox@gmail.com>
2026-03-04 15:57:50 +0900

    Update step08-rest-api-exception (전역예외 핸들러(@ControllerAdvice)) - view 연결

--- step08_rest-api-exception/src/main/java/com/spring/api/handler/GlobalAPIExceptionHandler.java
+++ step08_rest-api-exception/src/main/java/com/spring/api/handler/GlobalAPIExceptionHandler.java
@@ -12,7 +12,19 @@ import com.spring.api.dto.ErrorResponse;
 // ▶ 전역 예외 핸들러 
 
 @RestControllerAdvice  // REST API용 (= @ControllerAdvice + @ResponseBody)
-public class GlobalExceptionHandler {
+public class GlobalAPIExceptionHandler {
+	
+	
+	/*
+	 * 예외 발생 
+	 * 누가 잡아서 처리하느냐?
+	 * 1순위) try~catch 
+	 * 2순위) 컨트롤러의 @ExceptionHandler
+	 * 3순위) Advice의 @ExceptionHandler
+	 * 4순위) Spring 기본 예외 처리(BasicErrorController)
+	 * 
+	 */
+	
 	
 	@ExceptionHandler({IllegalArgumentException.class})
 	public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException e) {
---
+++ step08_rest-api-exception/src/main/java/com/spring/api/handler/GlobalExceptionHandler2.java
@@ -0,0 +1,39 @@
+package com.spring.api.handler;
+
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.spring.api.dto.ErrorResponse;
+
+// ▶ 전역 예외 핸들러 
+
+@ControllerAdvice           // MVC용
+public class GlobalExceptionHandler2 {
+	
+	
+	/*
+	 * 예외 발생 
+	 * 누가 잡아서 처리하느냐?
+	 * 1순위) try~catch 
+	 * 2순위) 컨트롤러의 @ExceptionHandler
+	 * 3순위) Advice의 @ExceptionHandler
+	 * 4순위) Spring 기본 예외 처리(BasicErrorController)
+	 * 
+	 */
+	
+	
+	@ExceptionHandler({IllegalArgumentException.class})
+	public String handleIllegalArgument(IllegalArgumentException e, Model model) {
+		model.addAttribute("exception",e.getMessage());
+		
+		
+		return "error";
+	}
+	
+}
