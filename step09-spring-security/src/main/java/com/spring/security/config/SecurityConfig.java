2e5a035a38a2844c213de0836b96aff9c881925b
Containerxox <containerxox@gmail.com>
2026-03-05 13:31:19 +0900

    Update steo09-spring-security (로그아웃)

--- step09-spring-security/src/main/java/com/spring/security/config/SecurityConfig.java
+++ step09-spring-security/src/main/java/com/spring/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.spring.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,6 +25,17 @@ public class SecurityConfig {
 									.loginPage("/login") // GET요청은 /login (뷰) 을 사용
 									.loginProcessingUrl("/login") //POST요청은 컨트롤러가 아닌 Spring Security 필터가 가로챔 -> Security 내부에서 UserDetailsService.loadUserByusername(useranme) 호출
 									.defaultSuccessUrl("/")); // 로그인 성공 시, /로 이동.
+		
+		http
+			.logout(logout ->
+								logout
+									.logoutUrl("/logout")
+									.logoutSuccessUrl("/")
+									.invalidateHttpSession(true) // 이전에 접속했었던 session 기록을 무효화
+									.deleteCookies("JSESSIONID") // 쿠키 삭제
+									.clearAuthentication(true) // security context를 초기화
+					);
+		
 		
 		return http.build();
 	}
