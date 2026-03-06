7c181e5e2d8772ae6cd37e18ae72651c04e0ae4c
Containerxox <containerxox@gmail.com>
2026-03-06 10:06:20 +0900

    Update steo10-token

--- step10-token/src/main/java/com/spring/token/config/SecurityConfig.java
+++ step10-token/src/main/java/com/spring/token/config/SecurityConfig.java
@@ -47,6 +47,31 @@ public class SecurityConfig {
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable);
 		
+		http
+			.authorizeHttpRequests(authorize -> authorize
+					
+					// 공개 페이지
+					.requestMatchers("/","/index","/signup","/about","/access-denied").permitAll()
+					// 공개 API
+					.requestMatchers("/api/v1/signup", "/api/v1/auth/**").permitAll()
+					// 정적 리소스 접근 허용
+					.requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+					
+					// USER 이상
+					.requestMatchers("/user", "/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+					// MANAGER 이상
+					.requestMatchers("/manager", "/api/v1/manager/**").hasAnyRole("MANAGER", "ADMIN")
+					// ADMIN 이상
+					.requestMatchers("/admin", "/api/v1/admin/**").hasAnyRole("ADMIN")
+					
+					// 그 외 모든 요청은 인증 처리가 완료되어야만 사용 가능
+					.anyRequest().authenticated()
+					
+					
+					
+					
+		);
+		
 			
 		return http.build();
 	}
