a3555f8389b2ac4fbbfdd84a67eedb74bf8ade4f
Containerxox <containerxox@gmail.com>
2026-03-10 17:13:45 +0900

    Update board

--- board/src/main/java/board/entity/User.java
+++ board/src/main/java/board/entity/User.java
@@ -1,18 +1,23 @@
 package board.entity;
 
+import org.springframework.data.domain.Persistable;
+
 import board.entity.constant.UserRoleType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString(callSuper = true)
 @Entity
-public class User extends AuditingFields {
+public class User extends AuditingFields implements Persistable<String>{
 	
 	@Id
 	@Column(name = "uid", length = 20)
@@ -30,6 +35,9 @@ public class User extends AuditingFields {
 	@Column(name = "role_type", columnDefinition = "VARCHAR(50)")
     private UserRoleType userRoleType;
     
+    @Transient
+    private boolean isNew = true;
+    
     protected User() {}
     
 	private User(String uid, String username, String password, String email, UserRoleType userRoleType) {
@@ -43,5 +51,15 @@ public class User extends AuditingFields {
 	public static User of(String userId, String username, String password, String email, UserRoleType userRoleType) {
 		return new User(userId, username, password, email, userRoleType);
 	}
+
+	@Override
+	public String getId() { return uid; }
+
+	@Override
+	public boolean isNew() { return isNew; }
+	
+	@PostPersist
+	@PostLoad
+	void markNotNew() { this.isNew = false; }
 	
 }
