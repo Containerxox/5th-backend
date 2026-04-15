f2f6cf5a7547fad52cc9610b5c6397cbb5cb818c
Containerxox <containerxox@gmail.com>
2026-04-15 15:42:08 +0900

    Add step13-local-file-sevice

---
+++ step13-local-file-service/src/main/java/file/Step13LocalFileServiceApplication.java
@@ -0,0 +1,13 @@
+package file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step13LocalFileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step13LocalFileServiceApplication.class, args);
+	}
+
+}
